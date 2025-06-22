d77adf00bd787d76c13a99520d02dd5c3eee07e7
Sayak Pan <sayakpan9635@gmail.com>
2025-06-23 01:04:05 +0530

    Update ALLOWED_HOSTS in settings.py for deployment

--- carbackend/settings.py
+++ carbackend/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-f(yq(oru%c4j%aq=n!*!)jra@ax+5ucg0)52972ql_y-c7*!3)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['carbackend-chha.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
